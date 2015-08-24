3c5842179c893ec2c4ab60aa147e930611b31aeb
ivandanov <ivandanov12@gmail.com>
2015-08-24 17:18:32 +0300

    NewestRecipes implemented, UI improvements, added category list in details category view

--- Source/RecipesHeaven.Web/Controllers/RecipeController.cs
+++ Source/RecipesHeaven.Web/Controllers/RecipeController.cs
@@ -1,13 +1,16 @@
 ﻿namespace RecipesHeaven.Web.Controllers
 {
+    using System.Linq;
     using System.Web.Mvc;
     using AutoMapper;
+    using AutoMapper.QueryableExtensions;
     using RecipesHeaven.Data.Contracts;
     using RecipesHeaven.Services.Contracts;
     using RecipesHeaven.Web.ViewModels.Recipe;
 
     public class RecipeController : BaseController
     {
+        private const int DefaultNewestRecipesCount = 9;
         private IRecipesServices recipeService;
 
         public RecipeController(IRecipesHeavenData data, IRecipesServices recipeService)
@@ -28,6 +31,14 @@
             var recipeModel = Mapper.Map<RecipeViewModel>(recipe);
 
             return View("RecipeDetails", recipeModel);
+        }
+
+        public ActionResult NewestRecipes(int count = DefaultNewestRecipesCount)
+        {
+            var recipes = this.recipeService.GetNewestRecipes(count)
+                .AsQueryable().Project().To<RecipeOverviewModel>().ToList();
+
+            return PartialView(recipes);
         }
     }
 }
