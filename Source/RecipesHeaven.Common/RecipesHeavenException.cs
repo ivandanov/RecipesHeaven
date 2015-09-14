05066033e926415ff0a7ed9f25bb4608ae194271
ivandanov <ivandanov12@gmail.com>
2015-09-14 15:01:45 +0300

    Added RecipesHeavenException.cs to handle application errors.

---
+++ Source/RecipesHeaven.Common/RecipesHeavenException.cs
@@ -0,0 +1,22 @@
+﻿namespace RecipesHeaven.Common
+{
+    using System;
+
+    public class RecipesHeavenException : Exception
+    {
+        public RecipesHeavenException(string message)
+            : base (message)
+        {
+        }
+
+        public RecipesHeavenException(string message, Exception innerException)
+            : base(message, innerException)
+        {
+        }
+
+        public RecipesHeavenException(string messageFormat, params object[] args)
+            : base(string.Format(messageFormat, args))
+        {
+        }
+    }
+}
